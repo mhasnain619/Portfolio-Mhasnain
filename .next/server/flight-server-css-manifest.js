c80f5252288ec85903419e2706612b3f3b8df2c5
Muhammad Hasnain <hasnain0893@gmail.com>
2024-07-05 09:15:08 +0500

    first commit

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "E:\\Codes\\REACT JS FULL COURSE\\Portfolio\\Portfolio-Mhasnain\\app\\page": [
+      "E:\\Codes\\REACT JS FULL COURSE\\Portfolio\\Portfolio-Mhasnain\\styles\\globals.css"
+    ]
+  },
+  "E:\\Codes\\REACT JS FULL COURSE\\Portfolio\\Portfolio-Mhasnain\\app\\layout.tsx": [
+    "E:\\Codes\\REACT JS FULL COURSE\\Portfolio\\Portfolio-Mhasnain\\styles\\globals.css"
+  ]
+}
